517fff49a994c838d052c72c714ecd2f43abd823
Eslam Abdelbasset <eslamelbazedy@gmail.com>
2022-09-06 23:13:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region test1
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Eng. Islam");
+            #endregion
+        } 
+    }
+}
