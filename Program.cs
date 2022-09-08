bbd3220fe53665b083b5be08d3f65a8c7ded50c6
Eslam Abdelbasset <eslamelbazedy@gmail.com>
2022-09-08 17:52:46 +0200

    casting

--- Program.cs
+++ Program.cs
@@ -4,12 +4,30 @@ namespace test_demo
 {
     class Program
     {
+         
         static void Main(string[] args)
         {
             #region test1
             //Console.WriteLine("Hello World!");
             //Console.WriteLine("Eng. Islam");
-            #endregion
+
+            //var x = 5;
+
+            //var time = new TimeSpan();
+            //Console.WriteLine(time);
+
+
+            //double pi = 3.14;
+            //Console.WriteLine(pi);
+
+            // Casting--------
+            //int x = 300;
+            //byte y = (byte)x;
+            //Console.WriteLine(y);
+            
+
+            #endregion 
+
         } 
     }
 }
